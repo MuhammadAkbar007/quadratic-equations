f36f079c59ed7a9e476a3b8bc17862d74f18174c
MuhammadAkbar007 <fotismenos007@mail.ru>
2023-10-30 22:12:17 +0500

    Came to answers section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
 const Header = () => {
   return (
-    <>
+    <div>
       <h1 className="mt-3">
-        Kvadrat tenglamalarni <span className="text-danger">Diskriminant</span>{" "}
-        usulda hisoblash
+        Kvadrat tenglamalarni{" "}
+        <span className="bold text-danger">Diskriminant</span> usulda hisoblash
       </h1>
 
       <h4 className="mt-2 text-secondary">
         Biz sizga{" "}
-        <span className="text-warning">
+        <span className="text-warning bold">
           {" "}
           &nbsp;&nbsp;ax<sup>2</sup> + bx + c = 0 &nbsp;
         </span>{" "}
         ko&apos;rinishidagi tenglamani yechishga yordam beramiz. <br /> Quyida
         a, b va c qiymatlarini kiritish kifoya:
       </h4>
-    </>
+    </div>
   );
 };
 
