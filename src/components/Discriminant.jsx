5c77e948d8fdc4c4915ad2804963d5360a0dc7b7
MuhammadAkbar007 <fotismenos007@mail.ru>
2023-10-31 21:36:55 +0500

    discriminant section added 🖩

---
+++ src/components/Discriminant.jsx
@@ -0,0 +1,20 @@
+const Discriminant = ({ a, b, c, disc }) => {
+  let tempA = a < 0 ? `(${a})` : `${a}`;
+  let tempC = c < 0 ? `(${c})` : `${c}`;
+
+  return (
+    <div>
+      <h2>
+        <br />
+        <span className="text-danger">Diskriminant</span> (D) ={" "}
+        <span className="text-warning">
+          b<sup>2</sup> - 4ac
+        </span>{" "}
+        = {b}
+        <sup>2</sup> - 4 * {tempA} * {tempC} = {disc}
+      </h2>
+    </div>
+  );
+};
+
+export default Discriminant;
