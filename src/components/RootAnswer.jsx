e8424b534c84b37502405e40a593fefc5ab4968c
MuhammadAkbar007 <fotismenos007@mail.ru>
2023-11-04 21:24:56 +0500

    displaying roots ✅

--- src/components/RootAnswer.jsx
+++ src/components/RootAnswer.jsx
@@ -1,11 +1,12 @@
-const RootAnswer = ({ val, sep, a, b, disc }) => {
-  console.log(b);
+const RootAnswer = ({ val, sep, a, b, disc, x1, x2 }) => {
   let tempB = b > 0 ? `-${b}` : `${b}`;
+  let tempA = a < 0 ? `-${a}` : `${a}`;
   if (val) {
     return (
       <h2>
         x<sub>1</sub> = x<sub>2</sub> ={" "}
-        <span className="text-warning">-b / (2 * a)</span> = {tempB}
+        <span className="text-warning">-b / (2 * a)</span> = {tempB} / (2 *{" "}
+        {tempA}) = {x1}
       </h2>
     );
   } else {
@@ -13,11 +14,43 @@ const RootAnswer = ({ val, sep, a, b, disc }) => {
       <div>
         {sep === 1 ? (
           <h2>
-            x<sub>1</sub> =
+            x<sub>1</sub> ={" "}
+            <span className="text-warning">
+              (-b +{" "}
+              <math>
+                <msqrt>
+                  <mi>D</mi>
+                </msqrt>
+              </math>
+              ) / (2 * a)
+            </span>{" "}
+            = ({b * -1} +{" "}
+            <math>
+              <msqrt>
+                <mi>{disc}</mi>
+              </msqrt>
+            </math>
+            ) / (2 * {tempA}) = {x1}
           </h2>
         ) : (
           <h2>
             x<sub>2</sub> ={" "}
+            <span className="text-warning">
+              (-b -{" "}
+              <math>
+                <msqrt>
+                  <mi>D</mi>
+                </msqrt>
+              </math>{" "}
+              / (2 * a)
+            </span>{" "}
+            = ({b * -1} -{" "}
+            <math>
+              <msqrt>
+                <mi>{disc}</mi>
+              </msqrt>
+            </math>
+            ) / (2 * {tempA}) = {x2}
           </h2>
         )}
       </div>
