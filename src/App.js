9ff3e64d7927070feb230b29780414cb160ffa88
MuhammadAkbar007 <fotismenos007@mail.ru>
2023-11-07 19:08:31 +0500

    author added ✍️

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Inputs from "./components/Inputs";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="container text-center">
       <Header />
       <Inputs />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="col-3 offset-8 my-5 pt-5">
+      <h5>
+        <i>Muallif : Akramjon Rahmonov</i>
+      </h5>
+    </div>
+  );
+};
+
+export default Footer;
