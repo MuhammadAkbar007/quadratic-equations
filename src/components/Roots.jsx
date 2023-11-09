c70c56a3ad681f9c6b43fa4e8b9fed498fdc4bbc
MuhammadAkbar007 <fotismenos007@mail.ru>
2023-11-09 21:03:34 +0500

    diskriminant ildizlar soni 📝

--- src/components/Roots.jsx
+++ src/components/Roots.jsx
@@ -14,6 +14,9 @@ const Roots = ({ disc, x1, x2, a, b, c }) => {
             = {b}
             <sup>2</sup> - 4 * {tempA} * {tempC} = {disc}
           </h2>
+          <h2 className="text-success">
+            Diskriminant 0 dan kichik bo'lgani uchun
+          </h2>
           <h1 className="text-danger">
             ⊘ Kvadrat tenglama yechimga ega emas ⊘
           </h1>
@@ -24,12 +27,16 @@ const Roots = ({ disc, x1, x2, a, b, c }) => {
             {disc === 0 ? (
               <div>
                 X<sub>1</sub> = X<sub>2</sub> = {x1}{" "}
-                <h3 className="text-secondary">(bitta ildiz)</h3>{" "}
+                <h3 className="text-secondary">
+                  (Diskriminant 0 ga teng bo'lganligi uchun bitta ildiz)
+                </h3>{" "}
               </div>
             ) : (
               <div>
                 X<sub>1</sub> = {x1}; X<sub>2</sub> = {x2}
-                <h3 className="text-secondary">(ikkita ildiz)</h3>
+                <h3 className="text-secondary">
+                  (Diskriminant 0 dan katta bo'lganligi uchun ikkita ildiz)
+                </h3>
               </div>
             )}
           </h1>
